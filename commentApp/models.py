6c64e563ea0f8fcf870d822fce3d57970b5d2baa
SteinsGate <kaermorhenxd@gmail.com>
2018-03-05 21:52:31 +0800

    [modify] models

--- commentApp/models.py
+++ commentApp/models.py
@@ -9,15 +9,17 @@ class Comment(MPTTModel):
     """评论信息数据库"""
     username = models.CharField(max_length=150, verbose_name='用户名')
     created_time = models.DateTimeField(auto_now_add=True, verbose_name='评论时间')
-    content = RichTextUploadingField(config_name='user')
+    content = RichTextUploadingField(config_name='user', verbose_name="评论内容")
 
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, blank=True, on_delete=models.CASCADE)
-    post = models.ForeignKey(settings.COMMENT_BIND_MODEL, null=True, blank=True, on_delete=models.CASCADE, verbose_name='评论的文章')
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, blank=True, on_delete=models.CASCADE,
+                             verbose_name="评论的用户")
+    post = models.ForeignKey(settings.COMMENT_BIND_MODEL, null=True, blank=True, on_delete=models.CASCADE,
+                             verbose_name='评论的文章')
     parent = TreeForeignKey('self', null=True, blank=True, on_delete=models.CASCADE, verbose_name='父级评论')
-
 
     class MPTTMeta:
         order_insertion_by = ['created_time']
+        verbose_name = verbose_name_plural = "用户评论"
 
     def __str__(self):
         if self.parent is not None:
